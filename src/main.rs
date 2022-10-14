b9de85da078d0f552d8d33641f1270135feedad8
tharaka tennakoon <tharakatennakoon@gmail.com>
2022-10-14 06:13:56 +0530

    move to greet bin

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,4 @@
-mod greetings;
-
-use greetings::{afternoon, morning};
 
 fn main() {
     println!("Hello, world!");
-
-    morning::formal::japanese();
-    morning::casual::japanese();
-
-    afternoon::formal::english();
-    afternoon::casual::english();
 }

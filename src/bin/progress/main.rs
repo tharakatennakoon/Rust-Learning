e6fac6d7900e90a6ae4459a744de9e90ac04729c
Tharaka Tennakoon <btnv78@zebra.com>
2022-11-03 17:56:09 +0530

    use \r in progress

--- src/bin/progress/main.rs
+++ src/bin/progress/main.rs
@@ -1,18 +1,18 @@
 mod progress;
 
+use std::collections::HashSet;
 use std::thread::sleep;
 use std::time::Duration;
-use std::collections::HashSet;
 
 use progress::Progress;
 use progress::ProgressIteratorExt;
 
-fn expensive_calculation<T>(_n:&T){
+fn expensive_calculation<T>(_n: &T) {
     sleep(Duration::from_secs(1));
 }
 
 fn main() {
-    let vec = vec![1,2,3,4,5];
+    let vec = vec![1, 2, 3, 4, 5];
     let mut hash = HashSet::new();
 
     hash.insert("nimala");
@@ -22,22 +22,27 @@ fn main() {
 
     println!("Progress ");
     progress::progress(vec.iter(), expensive_calculation);
+    println!("\n\n");
 
     println!("Progress Ex");
     progress::progress_ex(hash.iter(), expensive_calculation);
+    println!("\n\n");
 
     println!("Progress new");
     for n in Progress::new(vec.iter()) {
         expensive_calculation(n);
     }
+    println!("\n\n");
 
     println!("Progress attach to iterator");
     for n in vec.iter().progress() {
         expensive_calculation(n);
     }
+    println!("\n\n");
 
     println!("Progress with bound");
     for n in vec.iter().progress().with_bound() {
         expensive_calculation(n);
     }
+    println!("\n\n");
 }
--- src/bin/progress/progress.rs
+++ src/bin/progress/progress.rs
@@ -1,58 +1,78 @@
-const CLEAR : &str = "\x1B[2J\x1B[1;1H";
+use std::io::Write;
 
-pub fn progress<T, Iter>(iter : Iter, f : fn(&T)) 
-where Iter:Iterator<Item = T>{
+const CLEAR: &str = "\r";
+
+pub fn progress<T, Iter>(iter: Iter, f: fn(&T))
+where
+    Iter: Iterator<Item = T>,
+{
     let mut i = 1;
     for n in iter {
-        println!("{}{}", CLEAR, "*".repeat(i));
+        print!("{}{}", CLEAR, "*".repeat(i));
+        // print! send data to a buffer, and does not flush it to the output
+        // to improve performance. need to flush manually.
+        std::io::stdout().flush().unwrap();
         i += 1;
         f(&n);
     }
 }
 
 // same as progress, remove T
-pub fn progress_ex<Iter>(iter : Iter, f : fn(&Iter::Item)) 
-where Iter:Iterator{
+pub fn progress_ex<Iter>(iter: Iter, f: fn(&Iter::Item))
+where
+    Iter: Iterator,
+{
     let mut i = 1;
     for n in iter {
-        println!("{}{}", CLEAR, "*".repeat(i));
+        print!("{}{}", CLEAR, "*".repeat(i));
+        std::io::stdout().flush().unwrap();
         i += 1;
         f(&n);
     }
 }
 
 pub struct Progress<Iter> {
-    iter:Iter,
-    i:usize,
-    bound:Option<usize>
+    iter: Iter,
+    i: usize,
+    bound: Option<usize>,
 }
 
-impl<Iter> Progress<Iter>{
-    pub fn new(iter:Iter) -> Self {
-        Progress { iter, i: 0, bound: None }
+impl<Iter> Progress<Iter> {
+    pub fn new(iter: Iter) -> Self {
+        Progress {
+            iter,
+            i: 0,
+            bound: None,
+        }
     }
 }
 
-impl <Iter> Progress<Iter>
-where Iter:ExactSizeIterator{
+impl<Iter> Progress<Iter>
+where
+    Iter: ExactSizeIterator,
+{
     pub fn with_bound(mut self) -> Self {
         self.bound = Some(self.iter.len());
         self
     }
 }
 
-impl<Iter> Iterator for Progress<Iter> 
-where Iter: Iterator {
+impl<Iter> Iterator for Progress<Iter>
+where
+    Iter: Iterator,
+{
     type Item = Iter::Item;
 
     fn next(&mut self) -> Option<Self::Item> {
         print!("{}", CLEAR);
         match self.bound {
             Some(bound) => {
-                println!("[{}{}]", "*".repeat(self.i), " ".repeat(bound - self.i));
-            },
+                print!("[{}{}]", "*".repeat(self.i), " ".repeat(bound - self.i));
+                std::io::stdout().flush().unwrap();
+            }
             None => {
-                println!("{}", "*".repeat(self.i));
+                print!("{}", "*".repeat(self.i));
+                std::io::stdout().flush().unwrap();
             }
         }
         self.i += 1;
@@ -61,15 +81,16 @@ where Iter: Iterator {
 }
 
 // add to iterrator
-pub trait ProgressIteratorExt: Sized{
-    fn progress(self) -> Progress<Self>;    
+pub trait ProgressIteratorExt: Sized {
+    fn progress(self) -> Progress<Self>;
 }
 
 impl<Iter> ProgressIteratorExt for Iter
 // to block non iterable types
-where Iter:Iterator{
+where
+    Iter: Iterator,
+{
     fn progress(self) -> Progress<Self> {
         Progress::new(self)
     }
 }
-

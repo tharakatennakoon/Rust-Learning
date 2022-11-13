2c3109640e52f31674b36d9f61c4e8e2b8fbb6f1
tharaka tennakoon <tharakatennakoon@gmail.com>
2022-11-13 08:13:07 +0530

    return Option enum

--- src/median/mod.rs
+++ src/median/mod.rs
@@ -3,7 +3,7 @@ use std::ops::Div;
 use num_traits::FromPrimitive;
 use num_traits::Num;
 
-pub fn median<T>(mut num_array : Vec<T> ) -> T 
+pub fn median<T>(mut num_array : Vec<T> ) -> Option<T>
     // "Num" : This trait only allow numerical data tupes
     // "FromPrimitive" : used to convert 2 to T
     // find different between PartialOrd and Ord
@@ -13,17 +13,20 @@ pub fn median<T>(mut num_array : Vec<T> ) -> T
     //num_array.sort(); // This cannot be used as float types does not implement Ord trait
     num_array.sort_by(|a,b| a.partial_cmp(b).unwrap());
 
-    if len%2 != 0 {
+    if len == 0 {
+        return None;
+    }
+    else if len%2 != 0 {
         let index = len / 2;
         let median = num_array[index];
-        return median;
+        return Some(median);
     }
     else {
         let index1 = len / 2;
         let index2 = index1 - 1;
         
         let median = (num_array[index1] + num_array[index2]) / T::from_i32(2).unwrap();
-        return median;
+        return Some(median);
     }
 }
 
@@ -32,9 +35,20 @@ mod test_median {
     use super::median as test_fn;
 
     #[test]
+    fn test_epmpty_vec() {
+        let num:Vec<i32> = vec![];
+
+        let expected = None;
+
+        let output = test_fn(num);
+
+        assert_eq!(output, expected);
+    }
+
+    #[test]
     fn test_i32_vector_odd() {
         let num = vec![1,2,3];
-        let expected = 2;
+        let expected = Some(2);
 
         let output = test_fn(num);
 
@@ -44,7 +58,7 @@ mod test_median {
     #[test]
     fn test_i32_vector_even() {
         let num = vec![1,2,3,4];
-        let expected = 2;
+        let expected = Some(2);
 
         let output = test_fn(num);
 
@@ -54,7 +68,7 @@ mod test_median {
     #[test]
     fn test_f64_vector_odd() {
         let num = vec![1.0,2.0,3.0];
-        let expected = 2.0;
+        let expected = Some(2.0);
 
         let output = test_fn(num);
 
@@ -64,7 +78,7 @@ mod test_median {
     #[test]
     fn test_f64_vector_even() {
         let num = vec![1.0,2.0,3.0,4.0];
-        let expected = 2.5;
+        let expected = Some(2.5);
 
         let output = test_fn(num);
 
